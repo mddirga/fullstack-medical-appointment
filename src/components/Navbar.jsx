eea8edd512eba29dffc70da87ae4ba1333671c66
mddirga <madedirga22@gmail.com>
2025-07-26 08:03:05 +0800

    Navbar Section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { assets } from '../assets/assets';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+      <ul className="hidden md:flex items-start gap-5 font-medium">
+        <NavLink>
+          <li>Home</li>
+          <hr />
+        </NavLink>
+        <NavLink>
+          <li>All Doctors</li>
+          <hr />
+        </NavLink>
+        <NavLink>
+          <li>About</li>
+          <hr />
+        </NavLink>
+        <NavLink>
+          <li>Contact</li>
+          <hr />
+        </NavLink>
+      </ul>
+      <div>
+        <button>Register</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
